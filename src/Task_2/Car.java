cb068c84fe7092effd2f75dc9c62c68b7f5417fa
Michael <mcenjoy@gmail.com>
2020-01-19 16:30:52 +0200

    init commit

--- src/Task_2/Car.java
+++ src/Task_2/Car.java
@@ -1,4 +1,98 @@
 package Task_2;
 
-public class Car {
+class Car {
+    private int id;
+    private String brandOfCar;
+    private String modelOfCar;
+    private int yearOfCar;
+    private String colorOfCar;
+    private double priceOfCar;
+    private int registrationNumberOfCar;
+
+    //region  Constructor "Car"
+    public Car(int id, String brandOfCar, String modelOfCar, int yearOfCar, String colorOfCar, double priceOfCar, int registrationNumberOfCar) {
+        this.id = id;
+        this.brandOfCar = brandOfCar;
+        this.modelOfCar = modelOfCar;
+        this.yearOfCar = yearOfCar;
+        this.colorOfCar = colorOfCar;
+        this.priceOfCar = priceOfCar;
+        this.registrationNumberOfCar = registrationNumberOfCar;
+    }
+    //endregion
+
+    //region Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrandOfCar() {
+        return brandOfCar;
+    }
+
+    public void setBrandOfCar(String brandOfCar) {
+        this.brandOfCar = brandOfCar;
+    }
+
+    public String getModelOfCar() {
+        return modelOfCar;
+    }
+
+    public void setModelOfCar(String modelOfCar) {
+        this.modelOfCar = modelOfCar;
+    }
+
+    public int getYearOfCar() {
+        return yearOfCar;
+    }
+
+    public void setYearOfCar(int yearOfCar) {
+        this.yearOfCar = yearOfCar;
+    }
+
+    public String getColorOfCar() {
+        return colorOfCar;
+    }
+
+    public void setColorOfCar(String colorOfCar) {
+        this.colorOfCar = colorOfCar;
+    }
+
+    public double getPriceOfCar() {
+        return priceOfCar;
+    }
+
+    public void setPriceOfCar(int priceOfCar) {
+        this.priceOfCar = priceOfCar;
+    }
+
+    public int getRegistrationNumberOfCar() {
+        return registrationNumberOfCar;
+    }
+
+    public void setRegistrationNumberOfCar(int registrationNumberOfCar) {
+        this.registrationNumberOfCar = registrationNumberOfCar;
+    }
+    //endregion
+
+    //region toString
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Car{");
+        sb.append("id=").append(id);
+        sb.append(", brand='").append(brandOfCar).append('\'');
+        sb.append(", model='").append(modelOfCar).append('\'');
+        sb.append(", year=").append(yearOfCar);
+        sb.append(", color='").append(colorOfCar).append('\'');
+        sb.append(", price=").append(priceOfCar);
+        sb.append(", registerNumber=").append(registrationNumberOfCar);
+        sb.append('}');
+        return sb.toString();
+    }
+    //endregion
 }
--- src/Task_2/MainApp.java
+++ src/Task_2/MainApp.java
@@ -1,4 +1,100 @@
 package Task_2;
 
+import java.util.*;
+
 public class MainApp {
+    public static void main(String[] args) {
+        int choose = 0;   // variable for menu
+        int mYear;   // model year
+        int mPrice;  // model price of car we are looking for
+        Scanner in = new Scanner(System.in);    // our Scanner for switch
+        // array of objects (creating and initializing)
+        Car[] carlist = new Car[]{
+                new Car(1, "BMW", "", 1994, "black", 2300, 869735),  // 1st car
+                new Car(2, "AUDI", "", 2002, "yellow", 4000, 869736), // 2nd car
+                new Car(3, "Volvo","", 1999, "green", 3300, 869737), // etc...
+                new Car(4, "TOYOTA","", 2002, "purple", 5000, 869738),
+                new Car(5, "Ferrari","", 2005, "red", 10000, 869739),
+                new Car(6, "Ford","", 1967, "yellow", 8800, 869739),
+                new Car(7, "Aston Martin","", 2009, "black", 18000, 869740),
+                new Car(8, "Lada", "",2013, "white", 1800, 869741),
+                new Car(9, "Dodge","", 2009, "white", 6700, 869742),
+                new Car(10, "Mitsubishi","", 2008, "red", 3200, 869743),
+                new Car(11, "Volkswagen","", 2016, "grey", 9000, 869744),
+                new Car(12, "Lexus", "",2014, "blue", 11000, 869745),
+                new Car(13, "Mercedes-Benz", "",2016, "black", 22000, 869746),
+                new Car(14, "Jaguar", "",2015, "grey", 24000, 869747),
+                new Car(15, "Lamborghini","", 2016, "silver", 30000, 869748)
+        };
+        List<Car> resultList = new ArrayList<>();
+
+        do {
+            showMenu();
+
+            choose = in.nextInt();
+            switch (choose) {
+                // show all the cars
+                case 1:
+                    printCars(Arrays.asList(carlist));
+                    break;
+                // list of all cars that in use more then n years
+                case 2:
+                    System.out.println("Enter the number of years that the car is already operated");
+                    mYear = in.nextInt();
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTime(new Date());
+                    for (int i = 0; i < carlist.length; i++) {
+                        if (calendar.get(Calendar.YEAR) - mYear >= carlist[i].getYearOfCar()) {
+                            resultList.add(carlist[i]);
+                        }
+                    }
+                    showResult(resultList);
+                    break;
+                //  list of cars of some model year with price more then
+                case 3:
+                    System.out.println("Enter the model year");
+                    mYear = in.nextInt();
+                    System.out.println("Enter the price");
+                    mPrice = in.nextInt();
+                    for (int i = 0; i < carlist.length; i++) {
+                        if (carlist[i].getYearOfCar() == mYear && carlist[i].getPriceOfCar() > mPrice)
+                            resultList.add(carlist[i]);
+                    }
+                    showResult(resultList);
+                    break;
+                default:
+                    break;
+            }
+        } while (choose != 4);
+    }
+
+    private static void showMenu() {
+        System.out.printf("%10s \n", "Menu");
+        System.out.println("1. All cars");
+        System.out.println("2. Cars that in use more then n years");
+        System.out.println("3. Cars of some period price of which is more then");
+        System.out.println("4. Exit");
+    }
+
+    private static void showResult(List<Car> resultList) {
+        if (resultList.isEmpty()) {
+            System.out.println("Nothing found");
+        } else {
+            printCars(resultList);
+            resultList.clear();
+        }
+    }
+
+    private static void printCars(List<Car> carlist) {
+        System.out.printf("%-5s%-20s%-10s%-10s%-10s%-20s\n", "id", "brand", "year", "color", "price", "register number");
+        for (int i = 0; i < carlist.size(); i++) {
+            System.out.printf("%-5s", String.valueOf(carlist.get(i).getId()));
+            System.out.printf("%-20s", String.valueOf(carlist.get(i).getBrandOfCar()));
+            System.out.printf("%-10s", String.valueOf(carlist.get(i).getYearOfCar()));
+            System.out.printf("%-10s", String.valueOf(carlist.get(i).getColorOfCar()));
+            System.out.printf("%-10s", String.valueOf(carlist.get(i).getPriceOfCar()));
+            System.out.printf("%-20s", String.valueOf(carlist.get(i).getRegistrationNumberOfCar()));
+            System.out.println();
+        }
+    }
 }
